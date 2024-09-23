00fe3d3b5ba09f861ef59e5b95c51d7d8a0cb972
João Vicente <joao@javabeats.com.br>
2024-09-23 14:44:43 -0300

    feat: enhance adapter

--- blockchain/contracts/JoKenPoAdapter.sql
+++ blockchain/contracts/JoKenPoAdapter.sql
@@ -13,10 +13,51 @@ contract JoKenPoAdapter {
         owner = msg.sender;
     }
 
-    function upgrade(address newImplementation) external {
-        require(msg.sender == owner, "Youy do not have permission");
+    modifier restricted() {
+        require(msg.sender == owner, "You do not have permission");
+        _;
+    }
+
+    modifier upgraded() {
+        require(address(joKenPo) != address(0), "You must upgrade first");
+        _;
+    }
+    
+    function upgrade(address newImplementation) external restricted {
         require(newImplementation != address(0), "Empty address is not permitted");
 
         joKenPo = IJoKenPo(newImplementation);
     }
+
+    function getResult() external view upgraded returns (string memory) {
+        return joKenPo.getResult();
+    }
+
+    function getBid() external view upgraded returns (uint256){
+        return joKenPo.getBid();
+    }
+
+    function setBid(uint256 newBid) external upgraded restricted {
+        joKenPo.setBid(newBid);
+    }
+
+    function getCommission() external view upgraded returns (uint8){
+        return joKenPo.getCommission();
+    }
+
+    function setCommission(uint8 newCommission) external upgraded restricted {
+        joKenPo.setCommission(newCommission);
+    }
+
+    function getBalance() external view upgraded returns(uint){
+        return joKenPo.getBalance();
+    }
+
+    function play(JoKenPoLibrary.Options newChoice) external payable upgraded {
+        joKenPo.play{value: msg.value}(newChoice);
+    }
+
+    function getLeaderboard() external view upgraded returns(JoKenPoLibrary.Player[] memory) {
+        return joKenPo.getLeaderboard();
+    }    
 }
