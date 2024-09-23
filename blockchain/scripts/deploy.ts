b213cb46878df4edf90b592517e5cd0a0cfd5d55
João Vicente <joao@javabeats.com.br>
2024-09-23 17:31:56 -0300

    feat: adjust deploy script

--- blockchain/scripts/deploy.ts
+++ blockchain/scripts/deploy.ts
@@ -1,10 +1,18 @@
 import { ethers } from "hardhat";
 
 async function main() {
-    const contract = await ethers.deployContract("JoKenPo");
-    await contract.waitForDeployment();
-    const address = await contract.getAddress();
-    console.log(`Contract JoKenPo deployed at: ${address}`);
+    const implementation = await ethers.deployContract("JoKenPo");
+    await implementation.waitForDeployment();
+    const implementationAddress = await implementation.getAddress();
+    console.log(`Implementation Contract JoKenPo deployed at: ${implementationAddress}`);
+
+    const adapter = await ethers.deployContract("JoKenPoAdapter");
+    await adapter.waitForDeployment();
+    const adapterAddress = await adapter.getAddress();
+    console.log(`Adapter Contract JoKenPo deployed at: ${adapterAddress}`);
+
+    await adapter.upgrade(implementationAddress);
+    console.log("Adapter was upgraded.");
 }
 
 main().catch((error) => {
